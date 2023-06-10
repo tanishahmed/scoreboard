e8445712ffabc1fbb22b22b97cfe2bd66754df37
tanishahmed <72077718+tanishahmed@users.noreply.github.com>
2023-06-10 18:37:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let score = 0
+let scoredisply = document.getElementById("score")
+
+function addone() {
+     score += 1
+     
+     scoredisply.textContent = score
+    
+}
+function addtwo() {
+     score += 2
+     
+     scoredisply.textContent = score
+    
+}
+function addthree() {
+     score += 3
+     
+     scoredisply.textContent = score
+    
+}
+
+let away = 0 
+let awaydisplay = document.getElementById("away")
+
+function awayone() {
+    away += 1
+    awaydisplay.textContent = away
+}
+
+function awaytwo() {
+    away += 2 
+    awaydisplay.textContent = away
+}
+function awaythree() {
+    away += 3
+    awaydisplay.textContent = away
+}
+
+
